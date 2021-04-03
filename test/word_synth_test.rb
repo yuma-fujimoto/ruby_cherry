fb9faced5df49c91b3c2dab385ae12b83278f842
yuma-fujimoto <sirouma12@gmail.com>
2021-04-04 04:55:45 +0900

    add word_synth

---
+++ test/word_synth_test.rb
@@ -0,0 +1,37 @@
+require "minitest/autorun"
+require "./lib/effects"
+require "./lib/word_synth"
+
+class WordSynthTest < Minitest::Test
+  def test_play_without_effects
+    synth = WordSynth.new
+    assert_equal "Ruby is fun!", synth.play("Ruby is fun!")
+  end
+
+  def test_play_with_reverse
+    synth = WordSynth.new
+    synth.add_effect(Effects.reverse)
+    assert_equal "ybuR si !nuf", synth.play("Ruby is fun!")
+  end
+
+  def test_play_with_echo
+    synth = WordSynth.new
+    synth.add_effect(Effects.echo(2))
+    assert_equal "RRuubbyy iiss ffuunn!!", synth.play("Ruby is fun!")
+  end
+
+  def test_play_with_loud
+    synth = WordSynth.new
+    synth.add_effect(Effects.loud(1))
+    assert_equal "RUBY! IS! FUN!!", synth.play("Ruby is fun!")
+  end
+
+  def test_play_with_many_effects
+    synth = WordSynth.new
+    synth.add_effect(Effects.echo(2))
+    synth.add_effect(Effects.loud(3))
+    synth.add_effect(Effects.reverse)
+
+    assert_equal "!!!YYBBUURR !!!SSII !!!!!NNUUFF", synth.play("Ruby is fun!")
+  end
+end
